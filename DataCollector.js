34e9cf6847c991756f4116a6fa9dcb52c8a85cfb
aaron <aahuang@berkeley.edu>
2019-01-19 23:35:55 -0800

    decrease trigger to 10 min

--- DataCollector.js
+++ DataCollector.js
@@ -8,7 +8,7 @@ request.post('https://alexa-disaster.herokuapp.com/debug', (err, res, body) => {
 request('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson', { json: true }, (err, res, body) => {
     if (err) { return console.log(err); }
     for (var i = 0; i < body.features.length; i++) {
-        if (new Date().valueOf() - body.features[i].properties.time < 1440000) {
+        if (new Date().valueOf() - body.features[i].properties.time < 600000) {
             console.log(body.features[i]);
             sendNotification(body.features[i].properties.type, body.features[i].properties.mag)
             //responseObject.latestEventsArray.push({ type: body.features[i].properties.type, magnitude: body.features[i].properties.mag, location: { lat: body.features[i].geometry.coordinates[0], long: body.features[i].geometry.coordinates[1] } })
@@ -20,7 +20,7 @@ request('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geoj
         if (err) { return console.log(err); }
         parser.parseString(body, function (err, feed) {
             feed.items.forEach(function (entry) {
-                if (new Date().valueOf() - new Date(entry.isoDate).valueOf() < 900000000) {
+                if (new Date().valueOf() - new Date(entry.isoDate).valueOf() < 600000) {
                     console.log(entry);
                     sendNotification('fire');
                 }
@@ -40,7 +40,7 @@ function isWithinRadius(radius, triggerLoc, eventLoc) {
 }
 
 function sendNotification(type, magnitude) {
-    if (type == 'earthquake') {
+    if (type == 'earthquake' && magnitude >= 5.0) {
         request('http://api.notifymyecho.com/v1/NotifyMe?notification=' +
             encodeURIComponent("A magnitude " + 
                 magnitude + ' earthquake was reported near your contact, Sam. Should I call them and make sure they are ok?') 
