c7417a5ba5b88e2389a1e94d5401079eb10a615c
aaron <aahuang@berkeley.edu>
2019-01-20 06:01:36 -0800

    refine datacollector

--- DataCollector.js
+++ DataCollector.js
@@ -2,15 +2,22 @@ const request = require('request');
 let Parser = require('rss-parser');
 let parser = new Parser();
 var distance = require('gps-distance');
-var responseObject = { latestEventsArray: [] }
+var responseObject = { latestEventsArray: [] } // why?
 
 request('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson', { json: true }, (err, res, body) => {
     if (err) { return console.log(err); }
     for (var i = 0; i < body.features.length; i++) {
         if (new Date().valueOf() - body.features[i].properties.time < 600000) {
             console.log(body.features[i]);
-            sendNotification(body.features[i].properties.type, body.features[i].properties.mag)
-            //responseObject.latestEventsArray.push({ type: body.features[i].properties.type, magnitude: body.features[i].properties.mag, location: { lat: body.features[i].geometry.coordinates[0], long: body.features[i].geometry.coordinates[1] } })
+            let feature = body.features[i]
+            let properties = feature.properties
+            if(properties.mag>=4.5)
+                request.get('/user/contacts', (err, res, body)=>{
+                    let contacts = body
+                    contacts
+                        .filter(o=> distance(o.lat, o.lng, feature.geometry.coordinates[0],  feature.geometry.coordinates[1]) <= 160.0)
+                        .map(o=>sendNotification('earthquake', properties.mag, o.name))
+                })
         }
     }
 
@@ -21,7 +28,7 @@ request('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geoj
             feed.items.forEach(function (entry) {
                 if (new Date().valueOf() - new Date(entry.isoDate).valueOf() < 600000) {
                     console.log(entry);
-                    sendNotification('fire');
+                    sendNotification('fire'); // I have no idea if this works or not b/c it has never been triggered
                 }
             })
         })
