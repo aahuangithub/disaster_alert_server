f2aa424eebe44cd40c5d705306208bf6cc11ae60
aaron <aahuang@berkeley.edu>
2019-01-19 21:36:04 -0800

    fix

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const [dbuser, dbpw] = [process.env.dbuser, process.env.dbpw]
 app.use(express.static('./'))
 app.use(express.json())
 app.get('/test', function(req, res){
     res.send({"success": true})
+})
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, 'index.html'))
 })
 app.listen(process.env.PORT, function(){console.log('now listening on '+process.env.PORT)})
